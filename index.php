1fecd606a69d3b0522d4177d5adb35c50b179a6e
Bonyone <123076507+Bonyone@users.noreply.github.com>
2024-02-22 12:35:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="et">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Multicolor</title>
+    <style>
+        
+    </style>
+</head>
+
+<body>
+<!--<div id="video-container">
+        <video id="myVideo" width="80%" height="auto" autoplay muted loop>
+            <source src="pictures/multicolor Render.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>-->
+
+    <header>
+        <img src="logo/multicolor white.png" alt="Логотип" id="logo">
+        <nav>
+            <!--<a href="#">Avaleht</a>-->
+            <!--<a href="#">Meist</a>-->
+            <a href="#">Teenused</a>
+            <a href="#">Galerii</a>
+            <a href="#">Kontaktid</a>
+        </nav>
+
+        <a href="#" onclick="changeLanguage('ru')" class="language-link" id="ruLink">
+        <img src="logo/russia.png" alt="Rus" class="flag-icon">
+        </a>
+        <a href="#" onclick="changeLanguage('en')" class="language-link" id="enLink">
+        <img src="logo/united-kingdom.png" alt="eng"  class="flag-icon">
+        </a>
+
+        <button id="contact-button" onclick="openContactPage()">Võta ühendust</button>
+    </header>
+
+
+    <div id="gallery">
+        <div class="background-photo">
+            <img src="pictures/lg!yrd.jpg" alt="Фоновое фото">
+            <div class="photo-overlay">
+                <div class="photo-text">Jata oma riided<br> professionaalidele</div>
+                <p class="text-photo">Roivaste remonditookoda. Pakume ka<br> masintikkimist koikidele kangatuupidele</p>
+                <button id="gallery-button">Vaata galerii</button>
+            </div>
+        </div>
+    </div>
+
+<!-- первый блок с тремя фотографиями -->
+<div class="photo-container">
+    <div class="photo">
+        
+        <img src="pictures/remont.png" alt="Фото 1">
+        <div class="photo-circle">1</div>
+        <div class="photo-text1">Rõivaste remont</div>
+        <p>Aitame teil kasutada oma<br> lemmikrõivaid võimalikult kaua<br> ja saada ökoloogilisemaks.</p>
+    </div>
+    <div class="photo">
+        <img src="pictures/figura.png" alt="Фото 2">
+        <div class="photo-circle">2</div>
+        <div class="photo-text1">Rõivaeseme<br> sobitamine teie<br> figuurile</div>
+        <p>Rõivaste muutmine täiusliku<br> istuvuse ja mugavustunde<br> saavutamiseks.</p>
+    </div>
+    <div class="photo">
+        <img src="pictures/embroid.png" alt="Фото 3">
+        <div class="photo-circle">3</div>
+        <div class="photo-text1">Masintikkimine</div>
+        <p>Ettevõtte logo tikkimine riietele<br> või kingitus unikaalses stiilis.</p>
+    </div>
+</div>
+
+<div class="side-by-side-block">
+    <div class="image-container">
+        <img src="pictures/too.png" alt="Meie töökoja">
+    </div>
+    <div class="side-by-side-content">
+        <div class="side-by-side-text">
+            <h2>Meie töökoja</h2>
+            <h4>Meie meistril on kõrgharidus kergetööstuse<br> tehnikumis ja katkematu kogemus kerg- ja<br> ülerõivaste õmblemisel, töötamine pehme<br> mööbli ja nahast kotte valmistavates<br> ettevõtetes.</h4>
+            <p>2019. aastal, olles alustanud masintikkimist, sattus ta nii armunud<br> loomingulisse protsessi ja tikkimises peituvatesse võimalustesse,<br> et tüdines monotoonsest tööst ettevõttes. Nii et 2021. aastal avas<br> ta oma loomingulise visiooni realiseerimiseks ja üle 15 aasta<br> kestnud õmbluskogemuse kasutamiseks autoriõmblustöökoja.</p>
+        </div>
+        <button class="vaata-button">Vaata rohkem</button>
+    </div>
+</div>
+
+
+
+
+        <!-- блок с заголовком и текстом по середине блока -->
+        <div class="centered-block">
+            <h2>Miks just meie teenused?</h2>
+            <p>Õmblemine nõuab hoolt, täpsust ja professionaalsust. Meie teadmised erinevatest<br> õmblustehnikatest ja
+                 masinatikkimisest võimaldavad meil pakkuda kvaliteetset<br> teenust.</p>
+        </div>
+    </div>
+
+    <div class="photo-container1">
+            <div class="photo1">
+                <img src="pictures/zaplatka.png" alt="Фото 1">
+                
+                <div class="photo-text2">Erinevad kangad</div>
+                <p style="color:aliceblue;">Kõik tuntud kangatüübid on läbinud meie käsitööliste käed.</p>
+            </div>
+            <div class="photo1">
+                <img src="pictures/masqnka.png" alt="Фото 2">
+                <div class="photo-text2">Professionaalne varustus</div>
+                <p style="color:aliceblue;">Meil on kvaliteetse töö jaoks vajalik varustus.</p>
+            </div>
+            <div class="photo1">
+                <img src="pictures/igla.png" alt="Фото 3">
+                <div class="photo-text2">Käsitöö</div>
+                <p style="color:aliceblue;">Mõned tööd teeme käsitsi ja meile see meeldib.</p>
+            </div>
+            <div class="photo1">
+                <img src="pictures/noznicq.png" alt="Фото 3">
+                <div class="photo-text2">Uued tehnoloogiad</div>
+                <p style="color:aliceblue;">Meil on arvuti programmid masinatikkimise kujunduse loomiseks.</p>
+            </div>
+            <!--<div class="photo1">
+                <img src="pictures/maniken.png" alt="Фото 3">
+                <div class="photo-text2">Tagasiside</div>
+                <p style="color:aliceblue;">Kasutame kaasaegset tehnoloogiat klientidega suhtlemiseks.</p>
+            </div>-->
+        </div>
+
+
+        <div id="gallery3">
+        <div class="background-photo3">
+            <img src="pictures/lg!yrd.jpg" alt="Фоновое фото">
+            <div class="photo-overlay3">
+                <div class="photo-text3">MULTI STUUDIO</div>
+                <p class="text-photo3">Kui teil on küsimusi, võtke meiega ühendust</p>
+                <button id="gallery-button3">VÕTA ÜHENDUST</button>
+            </div>
+        </div>
+    </div>
+    
+
+    <footer>
+        <div class="footer-content">
+            <div class="footer-logo">
+                <!-- Замените src на путь к вашему логотипу -->
+                <img src="./logo/Multicolor-Logo-Main-Small.png" alt="Логотип">
+            </div>
+            <p style="font-size: 24px; color:aliceblue;">Avataud<br>E-R 11.00-18.00<br> L 11.00-15.00</p>
+            <div class="footer-data">
+                <p>Multicolor OÜ</p>
+                <p>Registrikood 16076235</p>
+                <p>Tartu mnt. 67 Tallinn, Eesti</p>
+                <p>Телефон: +372 524 7142</p>
+                <p>Email: info@multistuudio.ee</p>
+            </div>
+        </div>
+    </footer>
+    <p class="text-center">&copy; <?php echo date("Y"); ?> Все права защищены.</p>
+
+    <script src="js/index.js" defer></script>
+</body>
+</html>
